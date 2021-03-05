70b1be66197f96b3833f990657fc8f83be670023
hataloo <jacksandberg998@gmail.com>
2021-03-05 18:16:53 +0100

    Fixed the calculation of distances in mean_classification and added a variant without the cross_validation to compare results. No notable difference.

--- mean_classification.R
+++ mean_classification.R
@@ -9,14 +9,14 @@ removed_accuracy <- array(dim = c(K))
 remaining_accuracy <- array(dim = c(K))
 
 pb <- timerProgressBar(min = 0, max = K*class_index, style = 3)
-for(i in 1:K){
-  removed_data <- train_images[((total_number_of_images/K*(i-1)+1):(total_number_of_images/K*i)),,]/255
+for(validation_index in 1:K){
+  removed_data <- train_images[((total_number_of_images/K*(validation_index-1)+1):(total_number_of_images/K*validation_index)),,]/255
   dim(removed_data) <- c(test_images_per_validation, image_size^2)
-  removed_labels <- train_labels[((total_number_of_images/K*(i-1)+1):(total_number_of_images/K*i))]
+  removed_labels <- train_labels[((total_number_of_images/K*(validation_index-1)+1):(total_number_of_images/K*validation_index))]
   
-  remaining_data <- train_images[-((total_number_of_images/K*(i-1)+1):(total_number_of_images/K*i)),,]/255
+  remaining_data <- train_images[-((total_number_of_images/K*(validation_index-1)+1):(total_number_of_images/K*validation_index)),,]/255
   dim(remaining_data) <- c(training_images_per_validation, image_size^2)
-  remaining_labels <- train_labels[-((total_number_of_images/K*(i-1)+1):(total_number_of_images/K*i))]
+  remaining_labels <- train_labels[-((total_number_of_images/K*(validation_index-1)+1):(total_number_of_images/K*validation_index))]
   
   removed_distances <- array(dim = c(test_images_per_validation, length(class_names)))
   remaining_distances <- array(dim = c(training_images_per_validation, length(class_names)))
@@ -24,23 +24,26 @@ for(i in 1:K){
   
   for(class_index in 1:length(class_names)){
     temp <- apply((remaining_data[remaining_labels == class_index-1,]), c(2), mean)
-    dim(temp) <- c(image_size^2)
-    cross_val_means[i,class_index,] <- temp
-    removed_distances[, class_index] <- apply((removed_data-cross_val_means[i,class_index,])^2, c(1) ,sum)
-    remaining_distances[, class_index] <- apply((remaining_data-cross_val_means[i,class_index,])^2, c(1) ,sum)
+    cross_val_means[validation_index,class_index,] <- temp
+    for(sample_index in 1:test_images_per_validation){
+      removed_distances[sample_index, class_index] <- mean((removed_data[sample_index, ] - cross_val_means[validation_index, class_index,])^2)
+    }
+    for(sample_index in 1:training_images_per_validation){
+      remaining_distances[sample_index, class_index] <- mean((remaining_data[sample_index,] - cross_val_means[validation_index, class_index,])^2)
+    }
     setTimerProgressBar(pb, getTimerProgressBar(pb)+1)
   }
   removed_classification <- apply(removed_distances, c(1), which.min)-1
-  removed_accuracy[i] = mean(removed_classification == removed_labels)
+  removed_accuracy[validation_index] = mean(removed_classification == removed_labels)
   remaining_classification <- apply(remaining_distances, c(1), which.min)-1
-  remaining_accuracy[i] = mean(remaining_classification == remaining_labels)
+  remaining_accuracy[validation_index] = mean(remaining_classification == remaining_labels)
 }
+
+for(i in 0:9){print(sum(remaining_classification==i))}
+for(i in 0:9){print(sum(removed_classification==i))}
 
 print(removed_accuracy)
 print(remaining_accuracy)
-#Meh results, around 15% accurate.
-
-#Inspecting the result of the prints below, we see that
-#the classifier is heavily biased to some classes
-for(i in 0:9){print(sum(remaining_classification==i))}
-for(i in 0:9){print(sum(removed_classification==i))}
+#Impressive results, around 68% accurate.
+print(mean(abs(removed_accuracy- remaining_accuracy)))
+#The average accuracy barely differs. 
---
+++ mean_classification_no_cv.R
@@ -0,0 +1,31 @@
+library(pbapply)
+total_mean_images <- list()
+for(class_index in 1:length(class_names)){
+  total_mean_images[[class_index]] <- colMeans(train_vectors_by_class[[class_index]])
+}
+
+all_distances <- array(dim = c(10,6000,10))
+#[class_index_data,sample_index, distance_to_class_index]
+
+pb <- timerProgressBar(min = 0, max = 10*10*6000, style = 3)
+for(class_index in 1:length(class_names)){
+  for(distance_to_class_index in 1:length(class_names)){
+    for(sample_index in 1:6000){
+      all_distances[[class_index, sample_index, distance_to_class_index]] <- mean((train_vectors_by_class[[class_index]][sample_index,] - total_mean_images[[distance_to_class_index]])^2)
+      setTimerProgressBar(pb, 1+ getTimerProgressBar(pb))
+    }
+  }
+}
+
+classification_matrix <- array(dim = c(10, 6000))
+correct_classifications <- 0
+
+for(class_index in 1:length(class_names)){
+    for(sample_index in 1:6000){
+      classification_matrix[class_index, sample_index] <- which.min(all_distances[class_index, sample_index,])
+      if(classification_matrix[class_index, sample_index] == class_index){
+        correct_classifications <- 1 + correct_classifications
+      }
+    }
+}
+print(correct_classifications/number_of_images)
